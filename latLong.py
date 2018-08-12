68c43d2baff8eb76b99e0045c4756ddf0fb64177
Memhir-Yasue <eyasu.woldu@temple.edu>
2018-08-12 19:42:21 -0400

    Compare distance between multiple points

--- latLong.py
+++ latLong.py
@@ -3,10 +3,10 @@ from geopy.geocoders import GoogleV3
 from geopy.distance import geodesic
 # geolocator = Nominatim(user_agent="specify_your_app_name_here")
 geolocator = GoogleV3()
-# port_City = ["Port Massawa", "Port Tadjoura", "Port Assab", "Port Djibouti"]
-location = geolocator.geocode(" Port Tadjoura")
-port = location.latitude, location.longitude
-location = geolocator.geocode("Mer Awi")
-destination = location.latitude, location.longitude
-
-print(geodesic(port,destinationf).miles)
+port_CityList = ['Port Massawa', 'Port Tadjoura', 'Port Assab', 'Port Djibouti']
+for port_City in port_CityList:
+	location = geolocator.geocode(port_City)
+	port = location.latitude, location.longitude
+	location = geolocator.geocode("Mer Awi")
+	destination = location.latitude, location.longitude
+	print(port_City, geodesic(port,destination).miles)
