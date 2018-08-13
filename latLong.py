cbb669d57e72b15731f85377f06423806d3592f9
Memhir-Yasue <eyasu.woldu@temple.edu>
2018-08-12 22:07:52 -0400

    make sure cities are from same country

--- latLong.py
+++ latLong.py
@@ -1,22 +1,24 @@
 # from geopy.geocoders import Nominatim
 from geopy.geocoders import GoogleV3
 from geopy.distance import geodesic
+import config
 # geolocator = Nominatim(user_agent="specify_your_app_name_here")
-geolocator = GoogleV3()
+geolocator = GoogleV3(api_key=config.api_key)
 port_CityList = ['Port Massawa', 'Port Assab', 'Port Tadjoura' , 'Port Djibouti']
 tigray = ['Himora','Shire', 'Aksum', 'Adwa', 'Adigrat', 'Negash'
 		 'Mekele', 'Ambalage', 'Korem', 'Alamta']
 region = [tigray]
 ###############################
 # for state in region:
-# 	for city in state:		Basic Template for acessing cities from region
+# 	for city in state:		Basic Template for accessing cities from region
 # 		print(city)
 ##############################
-for state in region:
-	for city in state: # Destination city
+for state_Name in region:
+	for city in state_Name: # Destination city
+		dest_City = city + ", Ethiopia" # Make sure towns are in Ethiopia
 		for port_City in port_CityList:
 			location = geolocator.geocode(port_City)
 			port = location.latitude, location.longitude
-			location = geolocator.geocode(city)
+			location = geolocator.geocode(dest_City)
 			destination = location.latitude, location.longitude
 			print(city,'---->', port_City, geodesic(port,destination).miles)
