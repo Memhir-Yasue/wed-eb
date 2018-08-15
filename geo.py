6a90fdd886b92973706b200da025dcc8fbf20d82
Memhir-Yasue <eyasu.woldu@temple.edu>
2018-08-14 22:19:25 -0400

    Append city name to list, remove extra for loop in func_dest

--- geo.py
+++ geo.py
@@ -13,10 +13,15 @@ tigray = ['Himora','Shire']
 amhara = ['Weldiya', 'Mersa']
 region = [tigray, amhara]
 
+port_geocord = []
+destination_geocord = []
+straight_distance = []
+elevation = []
+city_name_list = []
 
 
 
-def func_port_geocord(port_geocord):
+def func_port_geocord():
 	for port_city in port_cityList:
 		if port_city == 'Port de Doraleh, Djibouti': 
 			start = '11.578054, 43.094573'
@@ -27,36 +32,34 @@ def func_port_geocord(port_geocord):
 			port_geocord.append(start)
 	return port_geocord
 
-
-def func_destination_geocord(destination_geocord):
+def append_city_name():
 	for state_Name in region:
 		for city in state_Name:
-			dest_city = city + ", Ethiopia"
-			destination = geolocator.geocode(dest_city)
-			end = destination.latitude, destination.longitude
-			destination_geocord.append(end)
-			print(city,destination_geocord)
+			city_name_list.append(city)
+	return city_name_list
+
+def func_destination_geocord():
+	for city in city_name_list:
+		dest_city = city + ", Ethiopia"
+		destination = geolocator.geocode(dest_city)
+		end = destination.latitude, destination.longitude
+		destination_geocord.append(end)
+		print(city,destination_geocord)
 	return destination_geocord
 
 
 
 def straight_distance_calculator(port_geocord, destination_geocord,straight_distance):
-	# for state_Name in region:
-	# 	dest_index = 0
-	# 	for city in state_Name:
-	# 		end = destination_geocord[dest_index]
-	# 		dest_index+=1
-	# 		port_geocord_index = 0
     for dest_cord in destination_geocord:
     	end = dest_cord
-    	for port_city in port_geocord:
-    		start = port_city # Get cord for ports
+    	for port_cord in port_geocord:
+    		start = port_cord # Get cord for ports
     		distance = geodesic(start,end).miles # calculate distance
     		straight_distance.append(distance)
-    		print('---->', port_city,distance) 
+    		print(dest_cord,'---->', port_cord,distance) 
     return straight_distance
 
-def elevation(elevation):
+def elevation():
 	ELEVATION_BASE_URL = 'http://maps.googleapis.com/maps/api/elevation/json?'
 	URL_PARAMS = "locations=%s,%s&sensor=%s" % (lati, lngi, "false")
 	url=ELEVATION_BASE_URL + URL_PARAMS
@@ -70,16 +73,10 @@ def elevation(elevation):
 
 # def driving_distance_calculator
 
-
-
-
 def main():
-	port_geocord = []
-	destination_geocord = []
-	straight_distance = []
-	elevation = []
-	func_port_geocord(port_geocord)
-	func_destination_geocord(destination_geocord)
-	straight_distance_calculator(port_geocord,destination_geocord,straight_distance)
+	# func_port_geocord()
+	append_city_name()
+	func_destination_geocord()
+	# straight_distance_calculator(port_geocord,destination_geocord,straight_distance)
 
 main()
